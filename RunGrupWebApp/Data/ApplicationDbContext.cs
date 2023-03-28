007ac1b340b47ea89e616db0bf7691ffda96b139
Rungrukuk <Rungrukuk@gmail.com>
2023-03-28 09:09:11 +0400

    Add project files.

---
+++ RunGrupWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunGrupWebApp.Models;
+
+namespace RunGrupWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Address> Adresses { get; set; }
+    }
+}
---
+++ RunGrupWebApp/Data/Enums/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGrupWebApp.Data.Enums
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        FiveK,
+        TenK,
+        HalfMarathon
+    }
+}
